8bdff813bce56b52b7ad5644a1c15c177c2850f7
NotAlex575 <dapontealessandro89gmail.com>
2025-09-07 11:37:22 +0200

    creata navbar (sostituito il nome Header con navbar)

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { NavLink } from "react-router-dom"
+
+const Header = () => {
+  return (
+    <header>
+      <div className='container row px-4 text-center'>
+        <h1>Questa è la Navbar!</h1>
+        <div className='row'>
+          <p>
+            <NavLink to="/">Homepage</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/contacts">Contacts</NavLink>
+          </p>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
