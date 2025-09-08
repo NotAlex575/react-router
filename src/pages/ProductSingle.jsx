f7eea1725c7ed50536088dcf8082f981d2165b56
NotAlex575 <dapontealessandro89gmail.com>
2025-09-08 17:47:02 +0200

    EXTRA: Aggiunto useNavigate nel ProductSingle

--- src/pages/ProductSingle.jsx
+++ src/pages/ProductSingle.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const ProductSingle = () => {
   const { id } = useParams ();
